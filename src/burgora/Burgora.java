f70f76cae80167eca77eeccd4c391e2e9e613097
nzeenath <nzeenath2002@gmail.com>
2026-09-24 17:13:37 +0530

    Add LoginForm class

--- src/burgora/Burgora.java
+++ src/burgora/Burgora.java
@@ -4,6 +4,7 @@
  */
 package burgora;
 
+import burgora.view.LoginForm;
 import burgora.view.MainDashBoardForm;
 
 /**
@@ -16,7 +17,8 @@ public class Burgora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new MainDashBoardForm().setVisible(true);
+//        new MainDashBoardForm().setVisible(true);
+        new LoginForm().setVisible(true);
     }
     
 }
